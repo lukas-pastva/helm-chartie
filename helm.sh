df0fa9319290ae00b2c95e22af7361081a3c950b
lukas-pastva <info@lukaspastva.sk>
2023-01-11 22:46:33 +0100

    feat: initial commit

--- helm.sh
+++ helm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 helm package charts/chart
-helm repo index --url https://github.com/lukas-pastva/helm-chartie --merge index.yaml .
+helm repo index --url https://lukas-pastva.github.io/helm-chartie --merge index.yaml .
